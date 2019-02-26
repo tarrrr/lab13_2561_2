6d5af41667d019d5edf89581ebab4c06625a26f9
tarrrr <46591190+tarrrr@users.noreply.github.com>
2019-02-26 17:32:33 +0700

    Add files via upload

---
+++ lab13_1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+void swap(T d[],int x,int y){
+    T temp = d[x];
+    d[x] = d[y];
+    d[y] = temp;
+}
+
+template <typename T>
+void insertionSort(T d[],int N){
+for(int i =1;i<N;i++){
+	int j=i;
+	for(int p=0;p<N;p++){
+		if(p==j)
+		cout<<"["<<d[p]<<"]"<<" ";
+		else cout<<d[p]<<" ";
+	}
+	cout<<"=>";
+	while(d[j]>d[j-1]&&j>0){
+		swap(d,j,j-1);
+		j--;
+	}
+	for(int p=0;p<N;p++){
+		if(p==j)
+		cout<<"["<<d[p]<<"]"<<" ";
+		else cout<<d[p]<<" ";
+	}
+	cout<<"\n";
+}
+
+}
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
---
+++ lab13_2.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+const int N = 5;
+
+void inputMatrix(double [][N]);
+
+void findLocalMax(const double [][N], bool [][N]);
+
+void showMatrix(const bool [][N]);
+
+int main(){
+	double A[N][N]; 
+	bool B[N][N]={}; 
+	cout << "Input the matrix.....\n";
+	inputMatrix(A);
+	findLocalMax(A,B);
+	cout << "Result is \n";
+	showMatrix(B);
+	return 0;
+}
+
+// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+
+void inputMatrix(double a[][N]){
+	for(int i=0;i<N;i++){
+		cout<<"Row"<<i+1<<": ";
+		for(int j=0;j<N;j++){
+			cin>>a[i][j];
+		}
+	}
+}
+	
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i=1;i<N-1;i++){
+		for(int j =1;j<N-1;j++){
+			if(a[i][j]>a[i][j+1]&&a[i][j]>a[i][j-1]&&a[i][j]>a[i+1][j]&&a[i][j]>a[i-1][j]){
+				b[i][j]=1;
+				b[i][j+1]=0;
+				b[i][j-1]=0;
+				b[i+1][j]=0;
+				b[i-1][j]=0;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool a[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout<<a[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	}
